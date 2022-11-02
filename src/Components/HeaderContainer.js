eaea542164fa780b0b253f4dc298ebbb298583d0
sinxenon4 <sinxenon4@gmail.com>
2022-11-03 01:57:56 +0200

    Initial commit

---
+++ src/Components/HeaderContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HeaderContainer() {
+    return (
+        <>
+            <h1>Create Demand</h1>
+            <p>Search the product you need here. Use tags to find any alternative. </p>
+        </>
+    )
+}
---
+++ src/Components/StepsContainer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StepsContainer() {
+    return (
+        <div>StepsContainer</div>
+    )
+}

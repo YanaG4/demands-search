a2cab223caeb55477428d255d5c302572cac4c69
sinxenon4 <sinxenon4@gmail.com>
2022-11-08 04:33:45 +0200

    Design improvement (search icon highlight on focus)

--- src/components/SearchContainer/SearchField/SearchField.js
+++ src/components/SearchContainer/SearchField/SearchField.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
@@ -8,6 +8,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import { setKeyWords, getProductsStatus, searchProducts } from '../../../features/products/productsSlice';
 
 export default function SearchField() {
+    const [searchColor, setSearchColor] = useState("#12B8FF")
     const dispatch = useDispatch()
     const status = useSelector(getProductsStatus)
     const searchBarHandling = (text) => {
@@ -15,13 +16,14 @@ export default function SearchField() {
             dispatch(setKeyWords(text))
         dispatch(searchProducts())
     }
+
     return (
         <Paper
             component="form"
             variant="outlined"
-            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: '100', height: 50 }}
+            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: '100', height: 54 }}
         >
-            <IconButton type="button" sx={{ p: '10px' }} aria-label="search" size="large " >
+            <IconButton type="button" sx={{ p: '10px', color: searchColor }} aria-label="search" size="large"  >
                 <SearchIcon fontSize="inherit" />
             </IconButton>
             <InputBase
@@ -29,6 +31,9 @@ export default function SearchField() {
                 placeholder="I’m looking for..."
                 inputProps={{ 'aria-label': 'search demand' }}
                 onChange={(e) => searchBarHandling(e.target.value)}
+                autoFocus
+                onBlur={() => setSearchColor("#8492A6")}
+                onFocus={() => setSearchColor("#12B8FF")}
             />
         </Paper>
     )
